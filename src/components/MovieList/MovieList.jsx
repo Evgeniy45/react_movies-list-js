8c4a7646f243c65a441b46fe4e650f54812eaa5d
Evgeniy45 <jekaolhovskii@gmail.com>
2026-04-25 23:09:32 +0300

    fixed code

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -4,7 +4,7 @@ import './MovieList.scss';
 export const MovieList = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => {
-      return <MovieCard movie={movie} />;
+      return <MovieCard key={movie.imdbId} movie={movie} />;
     })}
   </div>
 );
